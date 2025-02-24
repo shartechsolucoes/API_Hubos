7a1ee90ccf2a6c01b4a4e1440091b6259d487425
Edson Rodrigues <shartech.solucoes@gmail.com>
2025-02-24 14:38:43 -0300

    Correção no count dos dados excluindo itens inativos.

--- src/controllers/Dashboard/index.js
+++ src/controllers/Dashboard/index.js
@@ -4,9 +4,9 @@ const prisma = new PrismaClient();
 
 export const getDashboardData = async (req, res) => {
 	const [order, user, kit] = await prisma.$transaction([
-		prisma.order.count(),
-		prisma.user.count(),
-		prisma.kit.count(),
+		prisma.order.count({where: { active: true }}),
+		prisma.user.count({where: { active: true }}),
+		prisma.kit.count({where: { active: true }}),
 	]);
 
 	res.send({ order, user, kit });
