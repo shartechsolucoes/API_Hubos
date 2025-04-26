324d7d34cd8e9710708f0e03d6f5c35c55ed1707
Alexandre Oliveira <alexandre_osvaldo@hotmail.com>
2025-04-26 15:10:21 -0300

    return orders user dash

--- src/controllers/Dashboard/index.js
+++ src/controllers/Dashboard/index.js
@@ -3,10 +3,23 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const getDashboardData = async (req, res) => {
+	const { userId } = req.query;
+
+	const userDate = await prisma.user.findFirst({
+		where: { id: userId },
+	});
+
 	const [order, user, kit] = await prisma.$transaction([
-		prisma.order.count({where: { active: true }}),
+		prisma.order.count({
+			where: {
+				active: true,
+				...(userId && userId !== '' && userDate.access_level !== 0
+					? { userId }
+					: {}),
+			},
+		}),
 		prisma.user.count(),
-		prisma.kit.count({where: { active: true }}),
+		prisma.kit.count({ where: { active: true } }),
 	]);
 
 	res.send({ order, user, kit });
